b1577c3fca7c3c439f9c5c3ed8e9dc61975a773d
Reinaldo Haynes <rey.haynes@me.com>
2017-12-10 23:52:57 -0500

    Unit testing prep

---
+++ __test__/test.js
@@ -0,0 +1,8 @@
+import { expect } from 'chai'
+// import CoinMarketCapAPI from '../src'
+
+describe('CoinMarketCapAPI Method Testing:', function() {
+  it("1 = 1", function() {
+    expect(1).to.equal(1)
+  })
+})
